ca39b29e917e7b3f0067f025f49756bc424c071b
Alexandr <semakaleksandr2014@gmail.com>
2018-05-30 23:26:35 +0300

    added Semantic css

--- code/js/index.js
+++ code/js/index.js
@@ -2,6 +2,6 @@ import React from 'react';
 import ReactDOM from "react-dom";
 //npm run start
 ReactDOM.render(
-  <h2>test</h2>, 
+  <button className="ui primary button">Save </button>, 
   document.getElementById('fieldToShow')
 );
