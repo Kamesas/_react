bd1f6228a4cd9b4156da5ab67a2c8d43d1d7fbb1
Alexandr <semakaleksandr2014@gmail.com>
2018-05-30 23:04:51 +0300

    test

--- code/js/index.js
+++ code/js/index.js
@@ -2,6 +2,6 @@ import React from 'react';
 import ReactDOM from "react-dom";
 //npm run start
 ReactDOM.render(
-  <h2>Есть! Работает!!!</h2>, 
+  <h2>test</h2>, 
   document.getElementById('fieldToShow')
 );
