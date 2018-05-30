cfffb58fba20398216c667d895d84f5c7209bef9
Alexandr <semakaleksandr2014@gmail.com>
2018-05-31 00:00:55 +0300

    added folder reducers

--- code/js/index.js
+++ code/js/index.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import ReactDOM from "react-dom";
+import {createStore} from 'redux';
+import allReducers from './reducers'
 //npm run start
+
+const store = createStore();
+
 ReactDOM.render(
   <button className="ui primary button">Save </button>, 
   document.getElementById('fieldToShow')
---
+++ code/js/reducers/car.js
@@ -0,0 +1,28 @@
+export default function () {
+	return [
+		{
+			id: 1,
+			car: "Audi",
+			speed: 234.45,
+			weight: 1.4,
+			desc: "R8 это замечательный спортивный автомобиль!",
+			img: "http://pitstopcarmb.com/wp-content/uploads/2018/03/r8.jpg"
+		},
+		{
+			id: 2,
+			car: "BMW",
+			speed: 256.4,
+			weight: 2.4,
+			desc: "BMW это замечательный спортивный автомобиль!",
+			img: "https://www.bmw.ua/content/dam/bmw/common/all-models/i-series/i3/2017/model-card/BMW-i3s_ModelCard.png/_jcr_content/renditions/cq5dam.resized.img.585.low.time1503580407026.png"
+		},
+		{
+			id: 3,
+			car: "Nissan",
+			speed: 200.23,
+			weight: 1.6,
+			desc: "Nissan это замечательный спортивный автомобиль!",
+			img: "https://www.cstatic-images.com/car-pictures/xl/USC50NIS022B021001_2.png"
+		}
+	]
+}
